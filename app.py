b2e7aceb9a3ab18d2b8f0d785ffa8c60ddaad303
Lucian Marin <marin.lucian@gmail.com>
2018-10-21 12:32:47 +0300

    about and api v0

--- app.py
+++ app.py
@@ -9,6 +9,42 @@ app = Flask('newscafe')
 app.jinja_env.filters['hostname'] = hostname
 app.jinja_env.filters['date'] = date
 app.jinja_env.filters['shortdate'] = shortdate
+app.jinja_env.globals['v'] = 4
+
+
+@app.route('/api/0/frontpage/')
+def api_popular():
+    data = load_db()
+    entries = sorted(data.values(), key=lambda k: k['shares'] if 'shares' in k else 0, reverse=True)
+    return jsonify(entries[:15])
+
+
+@app.route('/api/0/recent/')
+def api_recent():
+    data = load_db()
+    entries = sorted(data.values(), key=lambda k: k['time'], reverse=True)
+    return jsonify(entries[:15])
+
+
+@app.route('/')
+def home():
+    data = load_db()
+    entries = sorted(data.values(), key=lambda k: k['shares'] if 'shares' in k else 0, reverse=True)
+    count = len(entries)
+    return render_template('base.html', entries=entries[:15], count=count, view='home')
+
+
+@app.route('/recent/')
+def recent():
+    data = load_db()
+    entries = sorted(data.values(), key=lambda k: k['time'], reverse=True)
+    count = len(entries)
+    return render_template('base.html', entries=entries[:15], count=count, view='last')
+
+
+@app.route('/about/')
+def about():
+    return render_template('about.html', view='about')
 
 
 @app.route('/debug/')
@@ -29,33 +65,3 @@ def debug():
             'no recent description': len(no_recent_description)
         }
     return jsonify(entries)
-
-
-@app.route('/api/popular/')
-def api_popular():
-    data = load_db()
-    entries = sorted(data.values(), key=lambda k: k['shares'] if 'shares' in k else 0, reverse=True)
-    return jsonify(entries[:50])
-
-
-@app.route('/api/recent/')
-def api_recent():
-    data = load_db()
-    entries = sorted(data.values(), key=lambda k: k['time'], reverse=True)
-    return jsonify(entries[:50])
-
-
-@app.route('/')
-def home():
-    data = load_db()
-    entries = sorted(data.values(), key=lambda k: k['shares'] if 'shares' in k else 0, reverse=True)
-    count = len(entries)
-    return render_template('base.html', entries=entries[:15], count=count, view='home')
-
-
-@app.route('/recent/')
-def recent():
-    data = load_db()
-    entries = sorted(data.values(), key=lambda k: k['time'], reverse=True)
-    count = len(entries)
-    return render_template('base.html', entries=entries[:15], count=count, view='last')
