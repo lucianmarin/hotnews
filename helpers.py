e4ef74a7396cceaab3f7b74e1cfd95a448a150c4
Lucian Marin <marin.lucian@gmail.com>
2018-12-22 22:36:42 +0200

    fixed html part

--- helpers.py
+++ helpers.py
@@ -56,7 +56,7 @@ def fetch_fb(link):
 
 def fetch_desc(link):
     r = requests.get(link, headers=HEADERS)
-    if '<html>' not in r.text:
+    if '</html>' not in r.text:
         return ''
     soup = BeautifulSoup(r.text, features="lxml")
     description = get_description(soup)
@@ -69,7 +69,7 @@ def fetch_desc(link):
 
 def fetch_paragraphs(link):
     r = requests.get(link, headers=HEADERS)
-    if '<html>' not in r.text:
+    if '</html>' not in r.text:
         return []
     soup = BeautifulSoup(r.text, features="lxml")
     return get_paragraphs(soup)
