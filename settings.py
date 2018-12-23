32bcab72ea4a14fff17753f7665b441e8901b62b
Lucian Marin <marin.lucian@gmail.com>
2018-12-24 01:59:21 +0200

    added product hunt

--- settings.py
+++ settings.py
@@ -16,7 +16,8 @@ FEEDS = [
     "http://feeds.feedburner.com/sub/qz",
     "http://feeds.feedburner.com/sub/npr",
     "http://feeds.feedburner.com/sub/atlantic",
-    "http://feeds.feedburner.com/sub/fastcompany"
+    "http://feeds.feedburner.com/sub/fastcompany",
+    "http://feeds.feedburner.com/sub/producthunt"
 ]
 
 LINKS = [
