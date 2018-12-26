595356408c631360418f564679a6a17d524808f6
Lucian Marin <marin.lucian@gmail.com>
2018-12-26 16:40:27 +0200

    proper division

--- filters.py
+++ filters.py
@@ -18,8 +18,8 @@ def shortdate(stamp):
     """Short time interval for a timestamp."""
     timestamp = datetime.utcfromtimestamp(stamp)
     delta = datetime.utcnow() - timestamp
-    minutes = round(delta.total_seconds() // 60)
-    hours = round(delta.total_seconds() // 3600)
+    minutes = round(delta.total_seconds() / 60)
+    hours = round(delta.total_seconds() / 3600)
     if not hours:
         return "{0}m".format(minutes)
     return "{0}h".format(hours)
