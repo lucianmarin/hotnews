16e198fbebb06361f6e69a546ece7fb024c1af6a
Lucian Marin <marin.lucian@gmail.com>
2018-12-26 16:26:24 +0200

    improved shortdate

--- filters.py
+++ filters.py
@@ -18,14 +18,8 @@ def shortdate(stamp):
     """Short time interval for a timestamp."""
     timestamp = datetime.utcfromtimestamp(stamp)
     delta = datetime.utcnow() - timestamp
-    miliseconds = int(delta.microseconds) // 1000
-    seconds = int(delta.total_seconds())
-    minutes = seconds // 60
-    hours = seconds // 3600
-    if not hours and not minutes and not seconds:
-        return "{0}ms".format(miliseconds)
-    if not hours and not minutes:
-        return "{0}s".format(seconds)
+    minutes = round(delta.total_seconds() // 60)
+    hours = round(delta.total_seconds() // 3600)
     if not hours:
         return "{0}m".format(minutes)
     return "{0}h".format(hours)
