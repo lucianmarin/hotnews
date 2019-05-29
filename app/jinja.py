36c072549f67bb43e8bdec21a004723ebe358988
Lucian Marin <marin.lucian@gmail.com>
2019-05-29 15:17:51 +0300

    fixed newscafe

--- app/jinja.py
+++ app/jinja.py
@@ -13,5 +13,5 @@ def environment(**options):
     env.filters['superscript'] = superscript
     # globals
     env.globals['reverse'] = reverse
-    env.globals['v'] = 13
+    env.globals['v'] = 14
     return env
